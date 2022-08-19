5c9f544910d0b75400d926ea1806efa1135d3624
Haley Hickey <hchickey96@gmail.com>
2022-08-19 15:43:50 -0500

    updates

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,36 @@
 // Put your code here
+
+console.log("My Enemies List!")
+console.log("----------------")
+
+const enemies = [
+    {
+        FirstName: "Joshua", LastName: "Flowers", Offenses: ["Being a jerk to me in elementary school",
+            "Not being nice to me in elementary school"], IsReallyHated: true
+    },
+
+    {
+        FirstName: "Darth", LastName: "Vader", Offenses: ["Cut off Luke's hand", "Murdered all those kids",
+            "Unkind management practices"], IsReallyHated: false
+    },
+
+    {
+        FirstName: "Betty", LastName: "Rudelady", Offenses: ["Phone calls in the theater", "Phone calls on the bus",
+            "Phone calls in line at the grocery store", "Poor conversationalist"], IsReallyHated: true
+    },
+
+    { FirstName: "Leon", LastName: "Peck", Offenses: ["Keeps giving me a hotplate"], IsReallyHated: false }
+]
+
+const getEnemies = (people) => {
+    for (const person of people) {
+        if (person.IsReallyHated === true) {
+            console.log(`${person.FirstName} ${person.LastName} (Really, really dislike!)`)
+        }
+        else {
+            console.log(`${person.FirstName} ${person.LastName}`)
+        }
+    }
+}
+
+getEnemies(enemies);
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,67 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+// Roll Die
+const rollDice = () => {
+    let value = Math.floor(Math.random() * 6) + 1
+    return value
+}
+
+for (let i = 0; i < 10; i++) {
+    let die1 = rollDice();
+    let die2 = rollDice();
+    let total = die1 + die2
+
+    switch (die1) {
+
+        case 1:
+            die1 = "one";
+            break;
+        case 2:
+            die1 = "two";
+            break;
+        case 3:
+            die1 = "three";
+            break;
+        case 4:
+            die1 = "four";
+            break;
+        case 5:
+            die1 = "five";
+            break;
+        case 6:
+            die1 = "six";
+            break;
+    }
+
+    switch (die2) {
+
+        case 1:
+            die2 = "one";
+            break;
+        case 2:
+            die2 = "two";
+            break;
+        case 3:
+            die2 = "three";
+            break;
+        case 4:
+            die2 = "four";
+            break;
+        case 5:
+            die2 = "five";
+            break;
+        case 6:
+            die2 = "six";
+            break;
+    }
+
+    let message = `${die1} + ${die2} === ${total}`
+    if (die1 === die2) {
+        message += " DOUBLES!";
+    }
+
+    console.log(message)
+}
