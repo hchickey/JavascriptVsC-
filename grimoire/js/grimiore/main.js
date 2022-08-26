4848400e6798e210c0e7b71d4b3a2db1c4813366
Haley Hickey <hchickey96@gmail.com>
2022-08-26 10:28:23 -0500

    jsVsC

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -3,35 +3,69 @@
 console.log("Do you believe in magic?");
 console.log("------------------------");
 
-const getAllSpells = () => {
-    const allSpells = [
-        {
-            Name: "Turn enemy into a newt",
-            IsEvil: true,
-            EnergyReqired: 5.1
-        },
-        {
-            Name: "Conjure some gold for a local charity",
-            IsEvil: false,
-            EnergyReqired: 2.99
-        },
-        {
-            Name: "Give a deaf person the ability to heal",
-            IsEvil: false,
-            EnergyReqired: 12.2
-        },
-        {
-            Name: "Make yourself emperor of the universe",
-            IsEvil: true,
-            EnergyReqired: 100.0
-        },
-        {
-            Name: "Convince your relatives your political views are correct",
-            IsEvil: false,
-            EnergyReqired: 2921.5
+
+const allSpells = [
+    {
+        Name: "Turn enemy into a newt",
+        IsEvil: true,
+        EnergyReqired: 5.1
+    },
+    {
+        Name: "Conjure some gold for a local charity",
+        IsEvil: false,
+        EnergyReqired: 2.99
+    },
+    {
+        Name: "Give a deaf person the ability to heal",
+        IsEvil: false,
+        EnergyReqired: 12.2
+    },
+    {
+        Name: "Make yourself emperor of the universe",
+        IsEvil: true,
+        EnergyReqired: 100.0
+    },
+    {
+        Name: "Convince your relatives your political views are correct",
+        IsEvil: false,
+        EnergyReqired: 2921.5
+    }
+
+]
+
+
+
+
+
+const goodSpellBook = (spells) => {
+    let goodBook = []
+
+
+    for (const spell of spells) {
+        if (spell.IsEvil === false) {
+            goodBook.push(spell.Name)
         }
+    }
 
-    ]
-
-    return allSpells;
+    return goodBook;
 }
+
+const evilSpellBook = (spells) => {
+    let evilBook = []
+
+
+    for (const spell of spells) {
+        if (spell.IsEvil === true) {
+            evilBook.push(spell.Name)
+        }
+    }
+
+    return evilBook;
+}
+
+
+
+console.log("Good Book")
+console.log(goodSpellBook(allSpells));
+console.log("Evil Book")
+console.log(evilSpellBook(allSpells));
